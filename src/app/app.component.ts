dc2d5bb2cb274291dcd916b5d8afe71fdcbc1f83
Bela <belacaste3@gmail.com>
2024-09-11 15:38:45 -0500

    migracion completada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/components/header/header.component';
 import { FooterComponent } from './layout/components/footer/footer.component';
-import { LoginComponent } from './auth/pages/login/login.component';
 import { HomeComponent } from './features/pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
